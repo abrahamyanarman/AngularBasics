9d8928ee6dc60f16993e4ca3e9ccc8a96fca0114
Arman <abrahamyan.arman.94@gmail.com>
2019-10-25 09:51:57 +0400

    login completed 5!

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {UsersService} from "../../shared/services/users.service";
+import {User} from "../../shared/models/user.model";
+import {Message} from "../../shared/models/message.model";
+import {AuthService} from "../../shared/services/auth.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+
+@Component({
+  selector: 'hm-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  form:FormGroup;
+  message:Message;
+
+  constructor(
+    private userService:UsersService,
+    private authService:AuthService,
+    private router:Router,
+    private rout:ActivatedRoute
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.message = new Message("denger","");
+    this.rout.queryParams.subscribe((params:Params)=>{
+      if (params['nowCanLogin']){
+        console.log(typeof params['nowCanLogin']);
+        this.showMessage('Now we can Login!','success');
+      }
+    })
+
+    this.form = new FormGroup({
+      'email':new FormControl(null,
+        [Validators.required,Validators.email]),
+      'password':new FormControl(null,
+        [Validators.required,Validators.minLength(6)])
+    });
+  }
+
+  private showMessage(text:string,type:string = 'danger'){
+    this.message = new Message(type,text);
+    window.setTimeout(()=>{
+      this.message.text = '';
+    },5000);
+  }
+
+  onSubmit(){
+    const formData = this.form.value;
+
+    this.userService.getUserByEmail(formData.email)
+      .subscribe((user:User)=>{
+
+        if (user){
+          if (user.password === formData.password){
+            this.message.text = '';
+            window.localStorage.setItem('user',JSON.stringify(user))
+            this.authService.login();
+            //this.router.navigate(['']);
+          }else {
+            this.showMessage("username or password is wrong!")
+          }
+        }else {
+          this.showMessage("User not exist!");
+        }
+      })
+  }
+
+}
---
+++ src/app/auth/registration/registration.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {UsersService} from "../../shared/services/users.service";
+import {User} from "../../shared/models/user.model";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'hm-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+
+  form:FormGroup;
+  constructor(
+    private userServicce:UsersService,
+    private router:Router
+  ) { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      'email': new FormControl(null,[Validators.required,Validators.email]),
+      'password': new FormControl(null,[Validators.required,Validators.minLength(6)]),
+      'name': new FormControl(null,[Validators.required]),
+      'agree': new FormControl(false,[Validators.requiredTrue])
+    })
+  }
+
+  onSubmit() {
+      const {email,password,name} = this.form.value;
+      const user = new User(email,password,name);
+    this.userServicce.createNewUser(user)
+      .subscribe((user:User)=>{
+          this.router.navigate(['/login'],
+            {
+              queryParams:{
+                nowCanLogin: true
+              }
+            })
+      });
+  }
+}
